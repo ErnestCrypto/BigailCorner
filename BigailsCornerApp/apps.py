ef4d19cb2d0c4b74871c3659d2218e1e48098dc5
ErnestCrypto <akotobamfo.eab@gmail.com>
2021-10-17 20:10:58 +0000

    init

---
+++ BigailsCornerApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BigailscornerappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'BigailsCornerApp'
---
+++ BigailsCornerApp/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+
+
+
+class StockTransaction(models.Model):
+    stock_price = models.CharField(max_length=30)
+    date_bought = models.DateField(max_length=10)
+    
+    def __str__(self):
+        return self.stock_price + ' : ' + self.date_bought
+    
+    
+    
+class Order(models.Model):
+    number_of_orders = models.CharField(max_length=10)
+    date_ordered = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.number_of_orders + ' : ' + self.date_ordered
+
+
+
+class Stock(models.Model):
+    stocktransaction = models.OneToOneField(StockTransaction,on_delete=CASCADE)
+    number_of_initial_stock = models.CharField(max_length=20)
+    date_stock_was_filled = models.CharField(max_length=10)
+    number_of_remaining_stock = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.number_of_initial_stock  + ' : ' + self.date_stock_was_filled + ' : '+ self.number_of_remaining_stock
+        
+
+
+
+
+class Product(models.Model):
+    stock = models.OneToOneField(Stock, on_delete=CASCADE)
+    total_number_of_products = models.CharField(max_length=20)
+    date_products_were_purchased = models.DateField(max_length=10)
+    products_left_in_store = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.total_number_of_products +' : '+self.date_products_were_purchased +' : ' + self.products_left_in_store 
+    
+    
+    
+    
+class Orderline(models.Model):
+    order = models.ForeignKey(Order,on_delete=CASCADE)
+    product = models.OneToOneField(Product, on_delete=CASCADE)
+    number_of_orderlines = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.product +' : '+ self.number_of_orderlines
+    
+    
+    
+    
+r
+    
+    
+    
